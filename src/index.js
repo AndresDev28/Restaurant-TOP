73a95c4297ca300ceafbce30012fd5a6ebc47398
AndresDev28 <andresdev28@hotmail.com>
2024-08-10 07:26:29 +0200

    Set up project structure with modules, styles, data and img folders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hola mundo!");
+import './style.css';
+//Importar imagen del logo
+import logoSrc from './img/hot-dog-logo.png';
+
+const logoDiv = document.getElementById('logo');
+
+// Crea un elemento de la imagen
+const logoImg = document.createElement('img');
+logoImg.src = logoSrc;
+logoImg.alt = 'Logo Restaurant';
+logoImg.classList.add('logo-img');
+
+logoDiv.appendChild(logoImg);
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', 
+      },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -15,4 +27,9 @@ module.exports = {
       inject: 'body',
     }),
   ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
 };
